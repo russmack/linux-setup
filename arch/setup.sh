1575102471c809e5573e0041a782903e575cfeab
Russell Mackenzie <russmackdev@gmail.com>
2019-12-22 00:00:28 +0000

    Initial commit.

---
+++ arch/setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+# This is a UK setup.
+# This is a BIOS setup, not UEFI.
+# This starts fdisk on /dev/sda.
+# This expects swap on /dev/sda1.
+
+
+echo "Setting keyboard layout"
+loadkeys uk
+
+fdisk /dev/sda
+
+mkfs.ext4 /dev/sda2
+mount /dev/sda2 /mnt
+
+mkswap /dev/sda1
+swapon /dev/sda1
+
+timedatectl set-ntp true
+
+sudo sed -i '/uk/!d' /etc/pacman.d/mirrorlist
+
+pacstrap /mnt base base-devel linux linux-firmware vim
+
+pacstrap /mnt netctl iw wpa_supplicant dialog wpa_actiond
+
+genstab -U /mnt >> /mnt/etc/fstab
+
+echo "Run arch-chroot /mnt"
+
